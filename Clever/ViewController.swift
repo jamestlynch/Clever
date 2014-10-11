8529af36df3345c2c42ffcfcb34500b0d05112eb
James Lynch <jamestly@usc.edu>
2014-10-11 12:49:03 -0700

    Initial commit. Base project based on "Empty Application" template.

---
+++ Clever/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clever
+//
+//  Created by James Lynch on 10/11/14.
+//  Copyright (c) 2014 James Lynch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
